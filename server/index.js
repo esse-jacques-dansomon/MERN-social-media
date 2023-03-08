a2407e5cae2f64dcc9f3cddf64a68bd57ca0403e
Ësse Jacques Dansomon <essedansomon@gmail.com>
2023-03-08 13:12:36 +0000

    setup post routes and add comments

--- server/index.js
+++ server/index.js
@@ -9,21 +9,36 @@ import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from 'url';
 import {register } from './src/controllers/auth.controller.js';
+import {createPost } from './src/controllers/post.controller.js';
 import authRoutes from './src/routes/auth.routes.js';
 import userRoutes from './src/routes/user.routes.js';
+import postRoutes from './src/routes/post.routes.js';
+import {authMiddleware} from "./src/middleware/auth.middleware.js";
 
-//CONFIG
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-dotenv.config();
+/*CONFIGURATIONS*/
+
+const __filename = fileURLToPath(import.meta.url) //get the current file path
+const __dirname = path.dirname(__filename); //get the current directory path
+dotenv.config(); //load the .env file
+
+//create the express app
 const app = express();
+
+//use the express app to parse json data from the request body
 app.use(express.json());
+//use the helmet package to secure the app by setting various HTTP headers
 app.use(helmet());
+//use the helmet package to secure the app by setting various HTTP headers
 app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin"}));
+//use the morgan package to log HTTP requests
 app.use(morgan("common"));
+//use the body-parser package to parse the request body
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
+//use the body-parser package to parse the request body
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+//use the cors package to allow cross-origin requests
 app.use(cors());
+//use the express app to serve static files from the public folder
 app.use("/static",express.static(path.join(__dirname, 'public/assets')));
 
 
@@ -40,12 +55,14 @@ const upload = multer({ storage: storage });
 
 
 
-//ROUTES
+/*ROUTES WITH POSTS*/
 app.post("/auth/register", upload.single("picture"), register);
+app.post("/posts", upload.single("picture"), createPost);
+
+/*ROUTES SET UP*/
 app.use("/auth", authRoutes);
 app.use("/users", userRoutes);
-
-
+app.use("/posts", postRoutes);
 
 
 // DB CONNECTION
@@ -57,10 +74,3 @@ mongoose.connect(process.env.MONGO_URL,
     }
 ).then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
 .catch((error) => console.log(error.message));
-
-
-
-
-
-
-
