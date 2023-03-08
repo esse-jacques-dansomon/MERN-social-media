1a332756ca5ec50640bab1d38ddca21e3ba88874
Ësse Jacques Dansomon <essedansomon@gmail.com>
2023-03-08 00:41:25 +0000

    rename files

--- index.js
+++ index.js
@@ -8,9 +8,9 @@ import helmet from "helmet";
 import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from 'url';
-import {register } from './controllers/auth.js';
-import authRoutes from './routes/auth.js';
-import {authMiddle} from "./middleware/auth.js";
+import {register } from './controllers/authController.js';
+import authRoutes from './routes/authRoutes.js';
+import {authMiddle} from "./middleware/authMiddleware.js";
 
 //CONFIG
 const __filename = fileURLToPath(import.meta.url);
