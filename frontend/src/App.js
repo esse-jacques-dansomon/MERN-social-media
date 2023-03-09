0345f1419b095cde154d380ad41d2096414bee50
Ësse Jacques Dansomon <essedansomon@gmail.com>
2023-03-09 05:52:11 +0000

    redux toolkit config

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,19 @@
-
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import HomePage from "pages/home/home.page";
+import LoginPage from "pages/login/login.page";
+import ProfilePage from "pages/profile/profile.page";
 
 function App() {
   return (
     <div className="App">
-
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<LoginPage />} />
+            <Route path="/home" element={<HomePage />} />
+            <Route path="/profile/:userId" element={<ProfilePage/>} />
+            <Route path="*" element={<h1>Page Not Found 404</h1>} />
+          </Routes>
+        </BrowserRouter>
     </div>
   );
 }
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,12 +3,19 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {Provider} from "react-redux";
+import store from "./redux/store";
+import {PersistGate} from "redux-persist/integration/react";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <Provider store={store.store}>
+            <PersistGate persistor={store.persistor} loading={null}>
+                <App/>
+            </PersistGate>
+        </Provider>
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
