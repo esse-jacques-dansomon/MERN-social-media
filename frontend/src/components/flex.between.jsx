9cbce83f171ea57fb5c855bfa2718dc33ab76d51
Ësse Jacques Dansomon <essedansomon@gmail.com>
2023-03-11 09:47:51 +0000

    add theme configuration

---
+++ frontend/src/components/flex.between.jsx
@@ -0,0 +1,10 @@
+import {Box, styled} from "@mui/material";
+
+const FlexBetween = styled(Box)({
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
+
+});
+
+export default FlexBetween;
