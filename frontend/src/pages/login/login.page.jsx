9873259e5e43c13e72882102eccae1f2d432f838
Ësse Jacques Dansomon <essedansomon@gmail.com>
2023-03-14 19:42:04 +0000

    add login page

--- frontend/src/pages/login/login.page.jsx
+++ frontend/src/pages/login/login.page.jsx
@@ -1,9 +1,28 @@
+import {Box, Typography, useMediaQuery, useTheme} from "@mui/material";
+import {useNavigate} from "react-router-dom";
+
 const LoginPage = () => {
-    return (
-        <div className="login-page">
-            <h1>Login Page</h1>
-        </div>
-    );
+    const theme = useTheme();
+    const isNonMobileScreens = useMediaQuery("(min-width: 1000px)");
+    const navigate = useNavigate();
+
+    return <>
+        <Box>
+            <Box width={"100%"} backgriundColor={theme.palette.background.alt}
+                 p={"1rem 6%"}  textAlign={"center"}>
+                <Typography
+                    fontWeight="bold"
+                    fontSize="32px"
+                    color="primary"
+                    onClick={() => navigate("/home")}>
+                </Typography>
+            </Box>
+
+            <Box width={"100%"} backgriundColor={theme.palette.background.alt} >
+                
+            </Box>
+        </Box>
+    </>
 }
 
 export default LoginPage;
